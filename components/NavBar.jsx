1fef946a86089d38f8a0aad0266d71054c7626e2
ravindu <ravindulakshitha250@gmail.com>
2024-10-17 01:32:55 +0530

    create navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,42 +1,74 @@
-import { useState } from "react";
-import "./NavBar.css";
+// import { right } from '@popperjs/core';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavbarCom = () => {
   return (
     <>
-      <Navbar expand="lg" className="custom-navbar">
-        <Container className="d-flex justify-content-between align-items-center">
-          <Navbar.Brand href="#home">
-            <b>Ravindu Lakshitha</b>
-          </Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            className="navbar-toggler" // Ensure this class is present
-            style={{color: "white"}}
-          />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
+      <Navbar collapseOnSelect expand="lg" className="bar">
+        <Container>
+          <Navbar.Brand
+            href="#home"
+            className="Ravi"
+            style={{
+              color: "#678e1d",
+              fontSize: "220%",
+              fontFamily: "-moz-initial",
+              letterSpacing: "10px"
+            }}
           >
-            <Nav>
-              <Nav.Link href="#home" style={{ color: "rgb(255, 241, 0)" }}>
-                <b>Portfolio</b>
+            <b>Ravindu</b>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              width="24"
+              height="24"
+              color="rgb(92.16% 95.69% 0%)"
+            >
+              <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+            </svg>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#portfolio"
+                style={{ color: "rgb(92.16% 95.69% 0%)" }}
+              >
+                Portfolio
               </Nav.Link>
-              <Nav.Link href="#link" style={{ color: "rgb(255, 241, 0)" }}>
-                <b>Projects</b>
+              <Nav.Link
+                href="#projects"
+                style={{ color: "rgb(92.16% 95.69% 0%)" }}
+              >
+                Projects
               </Nav.Link>
-              <Nav.Link href="#link" style={{ color: "rgb(255, 241, 0)" }}>
-                <b>Tech Stack</b>
+              <Nav.Link
+                href="#techstack"
+                style={{ color: "rgb(92.16% 95.69% 0%)" }}
+              >
+                Tech Stack
               </Nav.Link>
-              <Nav.Link href="#link" style={{ color: "rgb(255, 241, 0)" }}>
-                <b>Timeline</b>
+              <Nav.Link
+                href="#timeline"
+                style={{ color: "rgb(92.16% 95.69% 0%)" }}
+              >
+                Timeline
               </Nav.Link>
-              <Nav.Link href="#link" style={{ color: "rgb(255, 241, 0)" }}>
-                <b>Contact</b>
+              <Nav.Link
+                href="#contact"
+                style={{ color: "rgb(92.16% 95.69% 0%)" }}
+              >
+                Contact
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
